ef71b8aab5e9b3411fbb4e1b1f955d7911a6d8ce
ARAman7860 <khanbab7865431@gmail.com>
2026-01-17 15:14:18 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  name: "help",
+  run({ api, message }) {
+    api.sendMessage(
+      "🤖 Mira Bot Commands:\n\n!help → commands list\n!ping → test",
+      message.threadID
+    );
+  }
+};
