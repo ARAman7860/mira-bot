b196ff78210c63222146be5312d6d92ecb7b4123
ARAman7860 <khanbab7865431@gmail.com>
2026-01-18 09:28:03 +0530

    Update echo.js

--- commands/echo.js
+++ commands/echo.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  execute(senderId, sendMessage, args) {
+    if (!args || args.length === 0) {
+      return sendMessage(senderId, "❌ Kuch likho jise main repeat kar saku.");
+    }
+    sendMessage(senderId, args.join(" "));
+  }
+};
