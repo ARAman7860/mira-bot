5ae2f94030abe9083f047f54b323e724acbe8169
ARAman7860 <khanbab7865431@gmail.com>
2026-01-18 09:26:53 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  execute(senderId, sendMessage) {
+    sendMessage(
+      senderId,
+      "👋 Welcome!\nMain ek simple Messenger bot hoon.\nCommands dekhne ke liye 'help' likho."
+    );
+  }
+};
